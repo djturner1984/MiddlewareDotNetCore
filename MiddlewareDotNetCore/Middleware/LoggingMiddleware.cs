da074f86b2060a6104bb18280026d62506316a82
David Turner <djturner1984@gmail.com>
2018-08-05 21:36:46 +1000

    implemented an example middleware for dotnet core

---
+++ MiddlewareDotNetCore/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareDotNetCore.Middleware
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly LoggingOptions _options;
+
+        public LoggingMiddleware(RequestDelegate next, IOptions<LoggingOptions> options)
+        {
+            _next = next;
+            _options = options.Value;
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File("logs.log")
+                .CreateLogger();
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (_options.IsEnabled)
+            {
+                Log.Information($"{DateTime.Now}: Beginning request with {httpContext.Request.Path}");
+            }
+            await _next(httpContext);
+            if (_options.IsEnabled)
+            {
+                Log.Information($"{DateTime.Now}: Completed request with {httpContext.Request.Path}");
+            }
+        }
+    }
+
+    public class LoggingOptions
+    {
+        public bool IsEnabled { get; set; }
+    }
+}
